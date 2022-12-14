45444b4d2da692e891416fdc7df374e1f4491cc5
Josip Harci <harcijosip99@gmail.com>
2022-12-14 16:35:23 +0100

    Update

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const bill = document.getElementById('bill');
+const numberPeople = document.getElementById('numberPeople');
+const five = document.getElementById('five');
+const ten = document.getElementById('ten');
+const fifteen= document.getElementById('fifteen');
+const twentyfive = document.getElementById('twenty-five');
+const fifty = document.getElementById('fifty');
+const custom = document.getElementById('custom');
+const total = document.getElementById('total');
+const tipAmount = document.getElementById('tipAmount');
+const reset = document.getElementById('reset');
+
+
+function input{
+
+}
+
