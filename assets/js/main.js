98aaa4610fa005ab670fb969595a4686d5b9927c
Josip Harci <harcijosip99@gmail.com>
2022-12-16 23:47:26 +0100

    Update/fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,8 @@ const output1 = document.getElementById("tipAmount");
 const output2 = document.getElementById("total_a");
 const sub = document.getElementById("Enter");
 const num3 = document.getElementById("custom");
-const reset = document.getElementById("reset")
+const reset = document.getElementById("reset");
+const btn = document.getElementById("btn");
 
 
 
@@ -14,8 +15,6 @@ function myFunction(val) {
 }
 
 function task(a,b,c){
-    
-
     var a = num1.value;
     var b = num2.value;
     var c = num3.value / 100;
@@ -28,7 +27,23 @@ function task(a,b,c){
 
 sub.addEventListener('click', task);
 
-function onClick(event){
-    event.target.style.backgroundColor = 'hsl(172, 67%, 45%';
-    event.target.style.Color = '#000';
+function onClick(btn){
+    btn.target.style.backgroundColor = 'hsl(172, 67%, 45%';
+    btn.target.style.Color = '#000';
   }
+
+  function clearResult(){
+    output1.innerText = '$0.00';
+    output2.innerText = '$0.00';
+    num1.value = '';
+    num2.value = '';
+    num3.value = '';
+  }
+  
+  reset.addEventListener('click' , clearResult);
+
+  function sendform(inputvalue){
+    if (document.getElementById("bill").value !== "" && 
+     document.getElementById("numberPeople").value !== "" && document.getElementById("custom").value !== "") {
+     document.getElementById("Enter").click();
+}}
